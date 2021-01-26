784fcf2632da615ffde5732a389ccb7de987e0d6
Suleman Khan <suleman@invozone.com>
2021-01-26 17:22:02 +0500

    change port and remove Procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { spawn } = require('child_process');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // Serve gatsby build site
 app.use(express.static(`${__dirname}/public`));
